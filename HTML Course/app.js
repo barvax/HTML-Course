35e4c0a4ede77a62b50840261eb7de506e13b67a
barvax <69000340+barvax@users.noreply.github.com>
2024-02-03 17:57:55 +0200

    Delete HTML Course directory

--- HTML Course/app.js
+++
@@ -1,9 +0,0 @@
-let x =encodeURIComponent("/lesson 1/video/createFolder.mp4")
-const videoSource = document.getElementById('videoSource')
-
-
-function playVideo() {
-    videoSource.src = x;
-    console.log(videoSource.src);
-   // videoSource.play();
-}
